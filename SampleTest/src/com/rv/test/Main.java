68a38998805aee43d36eb66e8c4a31d455d1b3f6
HP PROBOOK G2 <HP PROBOOK G2@HPPROBOOKG2-PC>
2017-02-02 11:33:40 +0100

    Initial commit

---
+++ SampleTest/src/com/rv/test/Main.java
@@ -0,0 +1,16 @@
+package com.rv.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yes !!");
+		System.out.println("Jesus can !!");
+		System.out.println("I trust him !!");
+		 
+	}
+
+}
